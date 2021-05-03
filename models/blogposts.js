4767359dc9e537a5cdb97710e1f81de683a223f4
Natalie Finnegan <nfinneg2@gmail.com>
2021-05-03 13:45:24 -0600

    complete blog post Model

---
+++ models/blogposts.js
@@ -0,0 +1,38 @@
+const { Model, DataTypes } = require("sequelize");
+
+const sequelize = require("../config/connection");
+
+class BlogPosts extends Model {}
+
+BlogPosts.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "blog_posts",
+  }
+);
