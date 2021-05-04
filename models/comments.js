3e1239e4aee626d408cdc9fbae9b5a7b4b78bf19
Natalie Finnegan <nfinneg2@gmail.com>
2021-05-03 23:26:31 -0600

    added sequelize instance on comment model

--- models/comments.js
+++ models/comments.js
@@ -3,33 +3,42 @@ const sequelize = require("../config/connection");
 
 class Comments extends Model {}
 
-Comments.init({
-  id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  user_id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    references: {
-      model: "user",
-      key: "id",
+Comments.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+    comment: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    blog_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "blog_posts",
+        key: "id",
+      },
     },
   },
-  comment: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  blog_id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    references: {
-      model: "blogposts",
-      key: "id",
-    },
-  },
-});
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "comments",
+  }
+);
 
 module.exports = Comments;
