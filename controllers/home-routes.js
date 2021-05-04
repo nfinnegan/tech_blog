f45750dcff5bbbdc517171d5d37002a344e6b768
Natalie Finnegan <nfinneg2@gmail.com>
2021-05-04 12:13:14 -0600

    few hdbrs templates, made index rtes, strt homerte

---
+++ controllers/home-routes.js
@@ -0,0 +1,20 @@
+const router = require("express").Router();
+const { BlogPosts, User } = require("../models");
+
+// GET all Blog Posts for homepage
+router.get("/", async (req, res) => {
+  try {
+    const dbBlogPostData = await BlogPosts.findAll({
+      include: [{ model: User }],
+    });
+
+    const allBlogs = dbBlogPostData.map((post) => post.get({ plain: true }));
+    res.render("homepage", {
+      allBlogs,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
