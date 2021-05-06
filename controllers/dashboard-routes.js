4edb77731e20736502636fbc62ca14bcc9366732
Natalie Finnegan <nfinneg2@gmail.com>
2021-05-05 21:18:28 -0600

    wrking on dashboard route

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -3,19 +3,19 @@ const { BlogPosts } = require("../models");
 const withAuth = require("../utils/auth");
 
 // GET blog posts for dashboard when user is signed in
-router.get("/dashboard", withAuth, async (req, res) => {
+router.get("/", withAuth, async (req, res) => {
   try {
     const dbBlogPostData = await BlogPosts.findAll({
       where: {
-        username: req.body.username,
+        username: req.session.username,
       },
     });
-    // res.status(200).json(dbBlogPostData);
-    const allBlogs = dbBlogPostData.map((post) => post.get({ plain: true }));
-    res.render("dashboard", {
-      allBlogs,
-      loggedIn: req.session.loggedIn,
-    });
+    res.status(200).json(dbBlogPostData);
+    // const allBlogs = dbBlogPostData.map((post) => post.get({ plain: true }));
+    // res.render("dashboard", {
+    //   allBlogs,
+    //   loggedIn: req.session.loggedIn,
+    // });
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
